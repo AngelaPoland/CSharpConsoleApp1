ee43e8d46dcc90284e93756e71b915f7dd1ef7b7
apoland <@gettyimages.com>
2018-10-28 21:32:33 -0700

    practice with exceptions and try/catch

--- Program.cs
+++ Program.cs
@@ -10,10 +10,22 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            Person person1 = new Person();
-            Console.WriteLine(person1.getAge());
-            person1.setAge(29);
-            Console.WriteLine(person1.getAge());
+            try
+            {
+                string input = Console.ReadLine();
+
+                int convertedNum;
+                bool isConvertedSuccessfully = int.TryParse(input, out convertedNum);
+
+                if (!isConvertedSuccessfully)
+                {
+                    throw new Exception("Conversion not successful.");
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine("There was an error: {0}", ex.Message);
+            }
+            Console.WriteLine("The rest of my application is still running.");
         }
     }
 }
