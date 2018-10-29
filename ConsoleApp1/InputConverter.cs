ced2861855a522b4b570b8b4f34439b29012ad5c
apoland <@gettyimages.com>
2018-10-28 22:25:01 -0700

    had to move files around to properly version control on github - this is the simple calculator app Ive been working on

---
+++ ConsoleApp1/InputConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class InputConverter
+    {
+        public double ConvertInputToNumeric(string argTextInput)
+        {
+            double convertedNumber;
+            if (!double.TryParse(argTextInput, out convertedNumber)) throw new ArgumentException("Expected a numeric value.");
+            return convertedNumber;
+
+            //try
+            //{
+            //    int convertedNum;
+            //    bool isConvertedSuccessfully = int.TryParse(input, out convertedNum);
+
+            //    if (!isConvertedSuccessfully)
+            //    {
+            //        throw new Exception("Conversion not successful.");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    throw;
+            //}
+        }
+    }
+}
